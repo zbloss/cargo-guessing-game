030febff6d31de51aad0d341252b391d23beea1d
Zach Bloss <zacharybloss@gmail.com>
2021-09-09 21:34:58 -0400

    add ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let mut s = String::from("hello");
+    // s.push_str(", world!"); 
+    // println!("{}", s)
+    
+    let s = String::from("hello");
+    let s1 = s.clone();
+    println!("{}", s1)
+}
